253e365fb8deb388bc7426985f6196b595059efe
Sushil Jain <13596814+sushiljainam@users.noreply.github.com>
2023-01-27 15:46:36 +0530

    correct require

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const recJson = require('json/testData.json');
+const recJson = require('./json/testData.json');
+const locJson = require('./json/locales.json');
 const app = express();
 const PORT = process.env.PORT;
 
 app.get('/records', (req, res) => {
     // console.log(req.query);
     res.json(recJson.slice(0, Math.floor(Math.random() * (recJson.length))));
+});
+
+app.get('/locales', (req, res) => {
+    // console.log(req.query);
+    res.json(locJson);
 });
 // app.use(express.static('static'))
 
